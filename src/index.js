ad496279a7eda09b7f3c633cf42f27bec71bb2d1
Sultan <sultanp@gmail.com>
2021-01-18 22:59:56 +0000

    added todo input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import {ToDo} from './toDoClass';
+
+const addTodo = document.querySelector("#addtodo");
+const toDoTitle = document.querySelector("#todotitle");
+const toDoDescription = document.querySelector("#tododescription")
+const toDoDate = document.querySelector("#tododate")
+const toDoPriority = document.querySelector("#todopriority")
+const toDoForm =  document.querySelector("#todoform")
+
+let mylist = [];
+
+addTodo.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let newtodo = new ToDo(toDoTitle.value, toDoDescription.value, toDoDate.value, toDoPriority.value);
+
+    mylist.push(newtodo);
+
+    console.log(mylist);
+})
