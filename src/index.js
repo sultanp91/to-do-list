de3671247defcace9da296327ccdcc05d6e9b16b
Sultan <sultanp@gmail.com>
2021-01-19 01:16:00 +0000

    updated forms and data structure of project

--- src/index.js
+++ src/index.js
@@ -7,14 +7,24 @@ const toDoDate = document.querySelector("#tododate")
 const toDoPriority = document.querySelector("#todopriority")
 const toDoForm =  document.querySelector("#todoform")
 
-let mylist = [];
+const projectName = document.querySelector("#projectname")
+const addProject = document.querySelector("#addproject")
+const projectChoice = document.querySelector("#projectchoice");
+
+let mylist = [[]];
+let projectNames = ["Default"];
+
+
+
+
+
 
 addTodo.addEventListener("click", (e) => {
     e.preventDefault();
-
+    let projectIndex = projectChoice.value;
     let newtodo = new ToDo(toDoTitle.value, toDoDescription.value, toDoDate.value, toDoPriority.value);
 
-    mylist.push(newtodo);
-
+    mylist[projectIndex].push(newtodo);
+    toDoForm.reset();
     console.log(mylist);
 })
